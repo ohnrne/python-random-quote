1dc883e86488262af9ee31a5b8d3ca0920c7ad6a
ohnrne <85787891+ohnrne@users.noreply.github.com>
2021-06-23 20:02:55 +0100

    Explore4

--- get-quote.py
+++ get-quote.py
@@ -8,6 +8,9 @@ def main():
   last = len(quotes) - 1
   rnd = random.randint(0, last)
   print(quotes[rnd])
+  random.choice(quotes)
+  print random.choice(quotes)
+
 
 if __name__== "__main__":
   main()
